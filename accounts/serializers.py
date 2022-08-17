1d971fa13076a94235a1e81a756b23bda0a4386a
miyankouh <majid.miyankouh@gmail.com>
2022-08-17 10:57:32 +0430

    feat: create UserRegister view & path(register) & UserRegisterSerializer

---
+++ accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+def clean_email(value):
+    if 'admin' in value:
+        raise serializers.ValidationError('admin cant be email')
+
+class UserRegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True, validators=[clean_email])
+    password = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True)
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError('username cant be `admin`')
+        return value
+
+    def validate(self, data):
+        if data ['password'] != data['password2']:
+            raise serializers.ValidationError('passwords must math ')
+        return data
