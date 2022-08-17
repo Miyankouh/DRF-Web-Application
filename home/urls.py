decc9978a07215d2c98a192f4691a62f82b36e50
miyankouh <majid.miyankouh@gmail.com>
2022-08-17 16:53:15 +0430

    feat: add post, put, delete method & path(<int:pk>)

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # path("", views.home, name="home"),
     path("", views.Home.as_view(), name="home"), # endpoint
     path("questions/", views.QuestionView.as_view()), 
+    path("questions/<int:pk>/", views.QuestionView.as_view()), 
 
 ]
