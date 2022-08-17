dd7eabb50319a6803c7463476df772da91131c0a
miyankouh <majid.miyankouh@gmail.com>
2022-08-17 13:32:25 +0430

    feat: Using TokenAuthentication

--- A/settings.py
+++ A/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     # external apps
     'rest_framework',
+    'rest_framework.authtoken',
 
 ]
 
@@ -132,3 +133,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# REST_FRAMEWORK
+REST_FRAMEWORK = {
+     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+     ]
+}
